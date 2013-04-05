579708d046d0876dbf96cd289aa36893a7a6c315
Manuel Maly <manuel.maly@gmail.com>
2013-04-05 09:37:46 +0200

    switched to android 2.3.3 min target temporarily to enable strict mode for debugging

--- src/com/manuelmaly/hn/App.java
+++ src/com/manuelmaly/hn/App.java
@@ -1,6 +1,7 @@
 package com.manuelmaly.hn;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 import com.googlecode.androidannotations.annotations.EApplication;
@@ -8,26 +9,30 @@ import com.manuelmaly.hn.util.Const;
 
 @EApplication
 public class App extends Application {
-    
+
     private static App mInstance;
 
     @Override
     public void onCreate() {
         super.onCreate();
         mInstance = this;
-        
+
         GoogleAnalyticsTracker.getInstance().startNewSession(Const.GAN_ID, 30, this);
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().penaltyDeath()
+            .build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyLog().penaltyDeath().build());
     }
-    
+
     @Override
     public void onTerminate() {
         GoogleAnalyticsTracker.getInstance().stopSession();
-        
+
         super.onTerminate();
     }
-    
+
     public static App getInstance() {
         return mInstance;
     }
-    
+
 }
