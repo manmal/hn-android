4103808b1d8fe6f7ba68a5ef037da36ee779603d
Mark Dunne <mark.dunne02@gmail.com>
2013-04-06 17:32:50 +0100

    removed unused import

--- src/com/manuelmaly/hn/ArticleReaderActivity.java
+++ src/com/manuelmaly/hn/ArticleReaderActivity.java
@@ -16,7 +16,6 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
