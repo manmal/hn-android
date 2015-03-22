53efdfd681ca4015b4b35b238dc32ecceea2a8f2
Petar Petrov <petar@creativepragmatics.com>
2015-03-22 14:31:48 +0100

    starting crashlytics on launch
    When the app starts and if and only if the user
    has opted in for anonymous reporting, then
    the crashlytics library is started.

--- hn-android/src/com/manuelmaly/hn/ExternalIntentActivity.java
+++ hn-android/src/com/manuelmaly/hn/ExternalIntentActivity.java
@@ -1,37 +1,45 @@
 package com.manuelmaly.hn;
 
+import com.crashlytics.android.Crashlytics;
+import com.manuelmaly.hn.model.HNPost;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.manuelmaly.hn.model.HNPost;
-
 public class ExternalIntentActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        Uri uri = getIntent().getData();
-        String uriString = uri.toString().replaceAll("/$", "");
+    Uri uri = getIntent().getData();
+    String uriString = uri.toString().replaceAll("/$", "");
 
-        Intent i = null;
-        if (uriString.endsWith("news.ycombinator.com")) { // Front page
-            i = new Intent(this, MainActivity_.class);
-        } else if (uriString.contains("item")) { // Comment
-            String postId = uri.getQueryParameter("id");
-            HNPost postToOpen = new HNPost(uriString, null, null, null, postId, 0, 0, null);
-            i = new Intent(this, CommentsActivity_.class);
-            i.putExtra(CommentsActivity.EXTRA_HNPOST, postToOpen);
-        }
-
-        if (i != null)
-            startActivity(i);
-        else
-            Toast.makeText(this, "This seems not to be a valid Hacker News item!", Toast.LENGTH_LONG).show();
-
-        finish();
+    Intent i = null;
+    if (uriString.endsWith("news.ycombinator.com")) { // Front page
+      i = new Intent(this, MainActivity_.class);
+    } else if (uriString.contains("item")) { // Comment
+      String postId = uri.getQueryParameter("id");
+      HNPost postToOpen = new HNPost(uriString, null, null, null, postId, 0, 0, null);
+      i = new Intent(this, CommentsActivity_.class);
+      i.putExtra(CommentsActivity.EXTRA_HNPOST, postToOpen);
     }
+
+    if (i != null) {
+
+      if (!BuildConfig.DEBUG && Settings.isReportingAllowed(this)) {
+        Crashlytics.start(this);
+      }
+
+      startActivity(i);
+
+    } else {
+      Toast.makeText(this, "This seems not to be a valid Hacker News item!", Toast.LENGTH_LONG).show();
+    }
+
+    finish();
+  }
 }
